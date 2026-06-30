4614922c3d256966c587dd16e1000481f4b73a1d
vinay <vinaykhatana999@gmail.com>
2026-06-30 15:40:55 +0530

    Time: 0 ms (100%), Space: 21.7 MB (93.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expSum = n * (n + 1) / 2;
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        return expSum - sum;
+    }
+};

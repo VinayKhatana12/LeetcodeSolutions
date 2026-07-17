1b46aa17a70b0868b54c7566b3bf9e458a063d99
vinay <vinaykhatana999@gmail.com>
2026-07-17 18:17:44 +0530

    Time: 14 ms (74.9%), Space: 26 MB (49.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>map;
-        for(string s:strs){
+        for(string s :strs){
             string copy = s;
             sort(copy.begin(),copy.end());
             map[copy].push_back(s);
         }
         vector<vector<string>>ans;
-        for(auto entry:map){
-            ans.push_back(entry.second);
+        for(auto it:map){
+            ans.push_back(it.second);
         }
         return ans;
     }

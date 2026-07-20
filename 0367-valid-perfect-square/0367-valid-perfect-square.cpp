be4bcc620c85af444202dd3f9d6655698c2eae51
vinay <vinaykhatana999@gmail.com>
2026-07-20 22:45:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (54.09%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long l=0;
+        long h=num;
+        while(l<=h){
+            long mid = l+(h-l)/2;
+            if(mid*mid == num){
+                return true;
+            }
+            else if(mid*mid>num){
+                h=mid-1;
+            }
+            else {
+                l = mid+1;
+            }
+        }
+    return false;  
+    }
+};

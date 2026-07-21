b4c78168cdbb5c256a0e22747e343ae6a3bd1bc0
vinay <vinaykhatana999@gmail.com>
2026-07-21 12:50:37 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,8 +5,10 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
-                j++;
+                            j++;
+
             }
-        } 
+            
+        }
     }
 };

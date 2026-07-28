0b0e30587e1f80ed8c071cfe12e82caa9808cca1
vinay <vinaykhatana999@gmail.com>
2026-07-28 15:10:26 +0530

    Time: 0 ms (100%), Space: 10.1 MB (11.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char , int>map;
-        if(s.size()!=t.size())return false;
-        for(char ch: s){
-            map[ch]++;
-        }
-        for(char ch: t){
-            map[ch]--;
-        }
-        for(auto it:map){
-            if(it.second != 0){
-                return false;
-            }
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>map;
+        for(char ch:s) map[ch]++;
+        for(char ch:t) map[ch]--;
+        for(auto it : map){
+            if(it.second>0) return false;
         }
     return true;
-
-        
-        
     }
 };

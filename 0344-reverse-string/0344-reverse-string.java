bdd095205ca1db976289bb110245c120a47ccb89
vinay <vinaykhatana999@gmail.com>
2026-08-16 14:13:30 +0530

    Time: 0 ms (100%), Space: 48.2 MB (74.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -4,11 +4,10 @@ class Solution {
         int j=s.length-1;
         while(i<j){
             char temp = s[i];
-            s[i]=s[j];
-            s[j]=temp;
+            s[i]= s[j];
+            s[j]= temp;
             i++;
             j--;
-        }
-        
+        }  
     }
 }
